a5a08bb1acd9f9cf2cb4abf2101485a3271c8844
Nouran Abdelhady <nouranahady12@gmail.com>
2023-06-01 14:22:03 +0300

    update(logic): use logger and config.ini file

--- main.py
+++ main.py
@@ -1,100 +1,127 @@
 from bs4 import BeautifulSoup
+import configparser
+import logging
 from tqdm import tqdm
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-# Function to load the details
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+logger = logging.getLogger()
+
+
+# Function to load the details from the configuration file
 def load_details():
-    # Anghami playlist saved HTML file path
-    html_file_path = "./anghami.html"
-    # Spotify app details
-    client_id = "YOUR_CLIENT_ID"
-    client_secret = "YOUR_CLIENT_SECRET"
-    # Jupyter Notebook URL (Don't change this)
-    redirect_url = "http://127.0.0.1:8080"
-    # Spotify username
-    username = "YOUR_USERNAME"
-    # Spotify playlist name
-    spotify_playlist_name = "YOUR_PLAYLIST_NAME"
-    # Whether to save the playlist to a text file or not
-    save_to_text = True
-    txt_save_path = "./playlist.txt"
-    # The separator between the song name and the artist name in the text file
-    txt_song_artist_separator = ' - '
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    html_file_path = config.get('Anghami', 'html_file_path')
+    client_id = config.get('Spotify', 'client_id')
+    client_secret = config.get('Spotify', 'client_secret')
+    redirect_url = config.get('Spotify', 'redirect_url')
+    username = config.get('Spotify', 'username')
+    spotify_playlist_name = config.get('Spotify', 'playlist_name')
+    save_to_text = config.getboolean('General', 'save_to_text')
+    txt_save_path = config.get('General', 'txt_save_path')
+    txt_song_artist_separator = config.get('General', 'txt_song_artist_separator')
     return html_file_path, client_id, client_secret, redirect_url, username, spotify_playlist_name, save_to_text, txt_save_path, txt_song_artist_separator
 
-html_path, client_id, client_secret, redirect_url, username, spotify_playlist_name, save_to_text, txt_save_path, txt_song_artist_separator = load_details()
 
-# Read the HTML file and parse it using BeautifulSoup
-with open(html_path, encoding="utf8") as f:
-    content = f.read()
+def read_html_file(html_file_path):
+    with open(html_file_path, encoding="utf8") as f:
+        content = f.read()
+    return content
+
+
+def extract_songs_and_artists(content):
     soup = BeautifulSoup(content, 'html.parser')
+    class_lst = ["cell cell-title", "cell cell-title marquee"]
+    song_divs = soup.find_all("div", class_=class_lst)
+    artist_divs = soup.find_all("div", {"class": "cell cell-artist"})
+    songs = [div.find("span").text for div in song_divs]
+    artists = [div.text for div in artist_divs]
+    return songs, artists
 
-# Find the song titles from the HTML
-classLst = ["cell cell-title", "cell cell-title marquee"]
-mydivs = soup.find_all("div", class_=classLst)
-songs = []
-print("\nReading Songs")
-for job_element in tqdm(mydivs, bar_format="{l_bar}{bar}{r_bar}"):
-    title_element = job_element.find("span")
-    songs.append(title_element.text)
 
-# Find the artists from the HTML
-mydivsart = soup.find_all("div", {"class": "cell cell-artist"})
-artists = []
-print("\nReading Artists")
-for job_element in tqdm(mydivsart, bar_format="{l_bar}{bar}{r_bar}"):
-    artists.append(job_element.text)
-
-# Check if the number of songs matches the number of artists
-if len(songs) != len(artists):
-    print("\nError: Number of songs and artists do not match.")
-else:
-    print("\nReading successful")
-
-# Print the songs and artists
-print("\nPlaylist Details:")
-for i in range(len(songs)):
-    print(songs[i] + txt_song_artist_separator + artists[i])
-
-# Save the playlist to a text file
-if save_to_text:
+def save_playlist_to_text(songs, artists, txt_save_path, txt_song_artist_separator):
     with open(txt_save_path, 'w', encoding="utf8") as fp:
-        print("\nSaving playlist to text file...")
-        for i in tqdm(range(len(songs)), bar_format="{l_bar}{bar}{r_bar}"):
-            # Write each item on a new line
-            fp.write(songs[i] + txt_song_artist_separator + artists[i] + '\n')
-        print('\nPlaylist saved to text file.')
+        for song, artist in zip(songs, artists):
+            fp.write(f"{song}{txt_song_artist_separator}{artist}\n")
 
-# Authenticate and create a new playlist on Spotify
-scope = 'playlist-modify-public'
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
-                                               client_secret=client_secret,
-                                               redirect_uri=redirect_url,
-                                               scope=scope,
-                                               username=username))
-sp.user_playlist_create(user=username, name=spotify_playlist_name, public=True, description="Imported from Anghami")
 
-notfound = []
-playlist = sp.user_playlists(user=username)['items'][0]['id']
+def authenticate_spotify(client_id, client_secret, redirect_url, username):
+    scope = 'playlist-modify-public'
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
+                                                   client_secret=client_secret,
+                                                   redirect_uri=redirect_url,
+                                                   scope=scope,
+                                                   username=username))
+    return sp
 
-# Search and add tracks to the Spotify playlist
-print("\nImporting playlist to Spotify...")
-for i in tqdm(range(len(songs)), bar_format="{l_bar}{bar}{r_bar}", colour='green'):
-    try:
-        res = sp.search(q=songs[i] + " " + artists[i], type='track', limit=1)
-    except:
-        notfound.append(songs[i] + " " + artists[i])
-    else:
-        if len(res['tracks']['items']) == 0:
-            notfound.append(songs[i] + " " + artists[i])
-        else:
-            uri = res['tracks']['items'][0]['uri']
-            sp.user_playlist_add_tracks(user=username, playlist_id=playlist, tracks=[uri])
 
-print('\nPlaylist import completed.')
+def create_spotify_playlist(sp, username, spotify_playlist_name):
+    playlist = sp.user_playlist_create(user=username, name=spotify_playlist_name, public=True,
+                                       description="Imported from Anghami")
+    return playlist['id']
 
-if len(notfound) > 0:
-    print('\nThe following songs could not be found on Spotify:')
-    for song in notfound:
-        print(song)
+
+def search_and_add_tracks(sp, playlist_id, songs, artists, username):
+    not_found = []
+    for song, artist in tqdm(zip(songs, artists), total=len(songs), bar_format="{l_bar}{bar}{r_bar}", colour='green'):
+        try:
+            res = sp.search(q=f"{song} {artist}", type='track', limit=1)
+            if len(res['tracks']['items']) > 0:
+                uri = res['tracks']['items'][0]['uri']
+                sp.user_playlist_add_tracks(user=username, playlist_id=playlist_id, tracks=[uri])
+            else:
+                not_found.append(f"{song} {artist}")
+        except spotipy.SpotifyException as e:
+            logger.error(f"Spotify Error: {str(e)}")
+            not_found.append(f"{song} {artist}")
+
+    return not_found
+
+
+def main():
+    # Load details from configuration file
+    html_file_path, client_id, client_secret, redirect_url, username, spotify_playlist_name, save_to_text, txt_save_path, txt_song_artist_separator = load_details()
+
+    # Read the HTML file
+    content = read_html_file(html_file_path)
+
+    # Extract songs and artists from the HTML
+    songs, artists = extract_songs_and_artists(content)
+
+    # Check if the number of songs matches the number of artists
+    if len(songs) != len(artists):
+        logger.error("Error: Number of songs and artists do not match.")
+        return
+
+    # Print the playlist details
+    logger.info("\nPlaylist Details:")
+    for song, artist in zip(songs, artists):
+        logger.info(f"{song} {txt_song_artist_separator} {artist}")
+
+    # Save the playlist to a text file
+    if save_to_text:
+        save_playlist_to_text(songs, artists, txt_save_path, txt_song_artist_separator)
+        logger.info("Playlist saved to text file.")
+
+    # Authenticate and create a new playlist on Spotify
+    sp = authenticate_spotify(client_id, client_secret, redirect_url, username)
+    playlist_id = create_spotify_playlist(sp, username, spotify_playlist_name)
+
+    # Search and add tracks to the Spotify playlist
+    logger.info("Importing playlist to Spotify...")
+    not_found = search_and_add_tracks(sp, playlist_id, songs, artists, username)
+
+    logger.info("Playlist import completed.")
+
+    # Print the not found songs
+    if len(not_found) > 0:
+        logger.info("\nThe following songs could not be found on Spotify:")
+        for song in not_found:
+            logger.info(song)
+
+
+if __name__ == '__main__':
+    main()
